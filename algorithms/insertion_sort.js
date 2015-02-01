70a24e92b14b0753e1ae622b43c20ba60307b90d
Mattia Occhiuto <occhiutomattia@gmail.com>
2015-02-01 22:54:24 +0100

    removed log function

--- algorithms/insertion_sort.js
+++ algorithms/insertion_sort.js
@@ -1,17 +1,7 @@
-function log (log, msg) {
-  var msg = msg ||  '';
-  try {
-    console.log(msg, log);
-  } catch (e) {
-
-  }
-}
-
 var arrToSort = [5,2,4,6,1,3];
 
 // Insertion Sort algorithm - O(n*n) 
-console.time("insertion_sort_decr");
-var insertion_sort_incr = (function(a){
+var insertion_sort_incr = function(a){
   var iteration = 0;
   for(var i=1; i<a.length; i++){
     var k=a[i],
@@ -25,14 +15,9 @@ var insertion_sort_incr = (function(a){
     a[j+1]=k;
   }
 
-  log(a, 'increased');
-  log(iteration, 'increased iteration');
+}
 
-})(arrToSort);
-console.timeEnd("insertion_sort_decr");
-
-console.time("insertion_sort_decr");
-var insertion_sort_decr = (function(a){
+var insertion_sort_decr = function(a){
   var iteration = 0;
   for(var i=a.length-1; i>=0; i--){
     var k=a[i],
@@ -46,5 +31,4 @@ var insertion_sort_decr = (function(a){
     a[j-1]=k;
   }
 
-})(arrToSort);
-console.timeEnd("insertion_sort_decr");
+}
